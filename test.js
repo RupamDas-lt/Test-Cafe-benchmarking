7072bea59bffb0b9cfbb28c7b888d2ed80a37647
Rupam <rupamd@lambdatest.com>
2024-02-18 15:49:50 +0530

    Update test.js

--- test.js
+++ test.js
@@ -208,7 +208,7 @@ test('Open Internet Heroku APP Check boxes 2nd time', async t => {
 
 
     global.globalJsonObject.executionTime = seconds;
-    global.globalJsonObject.buildName = 'Testcafe_Sample_build_US_Jenkins';
+    global.globalJsonObject.buildName = 'Testcafe_Sample_build_US_Jenkins_Emulator';
     console.log(global.globalJsonObject);
 
     (async () => {
